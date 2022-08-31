bccee86a783071fd04a12868e7df575947b6d7cb
ncturner <nick@codescience.com>
2022-08-30 20:51:13 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.scss';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages';
+import Work from './pages/work';
+import About from './pages/about';
+import Topbar from './components/Topbar';
+import Navbar from './components/Navbar';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+      setIsOpen(!isOpen)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Topbar isOpen={isOpen} toggle={toggle} />
+      <Navbar toggle={toggle} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="work" element={<Work />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </Router>
   );
 }
 
